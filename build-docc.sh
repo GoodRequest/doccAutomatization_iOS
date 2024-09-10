7ded6ba334533b78f48c80933ef9e11b9e515749
Marek Vrican <marek.vrican@goodrequest.com>
2024-09-10 15:53:46 +0200

    fix: docc script

--- build-docc.sh
+++ build-docc.sh
@@ -1,3 +1,4 @@
+xcode-select -s /Applications/Xcode_15.0.app
 xcrun xcodebuild docbuild \
     -scheme GoodPersistence \
     -destination 'generic/platform=iOS Simulator' \
@@ -6,6 +7,4 @@ xcrun xcodebuild docbuild \
 xcrun docc process-archive transform-for-static-hosting \
     "$PWD/.derivedData/Build/Products/Debug-iphonesimulator/GoodPersistence.doccarchive" \
     --output-path ".docs" \
-    --hosting-base-path "doccAutomatization_iOS" # add your repo name later
-
-echo '<script>window.location.href += "/documentation/GoodPersistence"</script>' > .docs/index.html
+    --hosting-base-path "" # add your repo name later
